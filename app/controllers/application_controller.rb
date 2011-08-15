0ada0d8e68b062464ab46b697d4102033113bb67
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2011-08-15 07:35:11 -0300

    specifying where you will be directed to log into the system after

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :authenticate_user!
+
+protected
+  def after_sign_in_path_for(resource)
+    dashboard_path
+  end
 end
