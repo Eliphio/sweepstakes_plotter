d54a3325c58e66dd10b5f24d5210adbf9f8079a2
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2011-08-29 07:30:13 -0300

    redirects modifying and accepting the format js

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -1,3 +1,12 @@
 class BaseController < ApplicationController
   inherit_resources
+  respond_to :js
+
+  def create
+    create!{ collection_path }
+  end
+
+  def update
+    update!{ collection_path }
+  end
 end
