6904f9d93dd45149dd63de3551fe0d447e31fb33
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2011-08-29 07:39:44 -0300

    - Creating the hunches so create the bet - Creating scopes to locate the user's bets - Fixing the expression that creates the status of the helpers - Delegating the model Championship of the model round

--- app/models/bet.rb
+++ app/models/bet.rb
@@ -6,5 +6,20 @@ class Bet < ActiveRecord::Base
   validates :round, :user, :status, :presence => true
   validates :round_id, :uniqueness => {:scope => :user_id, :allow_blank => true}
 
-  has_enumeration_for :status, :with => Bet::Status, :create_helper => true
+  after_create :create_hunches
+
+  has_enumeration_for :status, :with => Bet::Status, :create_helpers => true
+
+  delegate :championship, :to => :round, :allow_blank => true
+
+  scope :of_round, lambda{|round_id| joins(:round).where(:round => {:id => round_id})}
+  scope :of_user, lambda{|user_id| joins(:user).where(:user => {:id => user_id})}
+  scope :hunchable, lambda { joins(:round => :games).where(:round => {:games => ({:game_day.gteq => Date.today}|{:game_day => Date.today, :game_time.gteq => 10.minutes.ago})}) }
+
+protected
+  def create_hunches
+    round.games.each do |game|
+      hunches.create :game => game
+    end
+  end
 end
