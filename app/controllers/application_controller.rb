13c629d1a04e3aef569a6725e030996bfc104aab
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2011-08-29 08:09:25 -0300

    - Assigning access denied message - Route after setting log

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,24 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   before_filter :authenticate_user!
+  rescue_from CanCan::AccessDenied, :with => :access_denied
 
 protected
+  def access_denied
+    flash[:error] = "You can't access this page!"
+    redirect_to root_path
+  end
+
   def after_sign_in_path_for(resource)
-    dashboard_path
+    if resource.admin?
+      dashboard_path
+    else
+      bet = Bet.hunchable.first
+      if bet
+        bet_path(bet)
+      else
+        hunchables_championships_path
+      end
+    end
   end
 end
