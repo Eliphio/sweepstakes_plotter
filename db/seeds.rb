d5096a6ad19fbb33371a2cc03a7bda3181d3b36d
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2011-08-31 14:32:21 -0300

    create seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+User.create! :nickname => "admin", :email => "admin@admin.com.br", :password =>  "adminwjfcmc", :password_confirmation => "adminwjfcmc", :profile => User::Profile::ADMIN
